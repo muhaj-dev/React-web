f20399f96a6c0d7e1cbc60ea9e8707bfc768b0f5
muhammod1 <muhammodholawale@gmail.com>
2022-03-08 22:43:29 +0100

    creating-components

---
+++ src/components/infoSection/Data.js
@@ -0,0 +1,50 @@
+export const homeObjOne = {
+    id: 'about',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Premium Bank',
+    headLine: 'Unlimited Transaction with zero fees',
+    description: 'Get access to our exclusive app that allows you to send unlimited transaction without getting charged any fees.',
+    buttonLabel: 'Get started',
+    imgStart: false,
+    img: require('../../images/img-1.png'),
+    alt: 'car',
+    dark: true,
+    primary: true,
+    darkText: false
+}
+
+export const homeObjTwo = {
+    id: 'discover',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Unlimeted Access',
+    headLine: 'Load to your account at my time',
+    description: 'We have covered no matter where you are located. All you need is an internet connection and a phone or computer.',
+    buttonLabel: 'Learn More',
+    imgStart: true,
+    img: require('../../images/img-2.png'),
+    alt: 'Piggybank',
+    dark: false,
+    primary: false,
+    darkText: true
+}
+
+export const homeObjThree = {
+    id: 'signup',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Join our Team',
+    headLine: 'Create an account is extremely easy',
+    description: 'Get access to our exclusive app that allows you to send unlimited transaction without getting charged any fees.',
+    buttonLabel: 'Start Now',
+    imgStart: false,
+    img: require('../../images/img-3.png'),
+    alt: 'car',
+    dark: false,
+    primary: false,
+    darkText: true
+}
